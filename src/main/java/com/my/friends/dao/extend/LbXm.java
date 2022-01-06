273992934a90632498df8ec716e7c9decfcd2359
“qinzh” <qingzhihao14@163.com>
2022-01-07 01:40:46 +0800

    提交3

--- src/main/java/com/my/friends/dao/extend/LbXm.java
+++ src/main/java/com/my/friends/dao/extend/LbXm.java
@@ -6,12 +6,6 @@ import lombok.Data;
 
 import java.util.List;
 
-/**
- * @Atuhor: qin
- * @Create: 2021-02-27-17-01
- * @Time: 17:01
- * @Description:
- */
 public class LbXm extends Lb {
 
     private List<LbItem> lbItem;
