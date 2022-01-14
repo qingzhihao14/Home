5161511b684bcd42d52ed866898e72d4b2c53b0c
qingzhihao14 <qinzh14@163.com>
2022-01-14 11:40:05 +0800

    SSL设置成功

--- src/main/java/com/my/friends/FriendsApplication.java
+++ src/main/java/com/my/friends/FriendsApplication.java
@@ -1,16 +1,17 @@
 package com.my.friends;
 
+
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -27,43 +28,51 @@ public class FriendsApplication{
     public static void main(String[] args) {
         SpringApplication.run(FriendsApplication.class,args);
     }
-    //重写configure方法，否则在部署到tomcat时，接口将访问不到
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return super.configure(builder);
-//    }
-//
-//    /**
-//     * http重定向到https
-//     * @return
-//     */
+
+
 //    @Bean
 //    public TomcatServletWebServerFactory servletContainer() {
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-//            @Override
-//            protected void postProcessContext(org.apache.catalina.Context context) {
-//                SecurityConstraint constraint = new SecurityConstraint();
-//                constraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                constraint.addCollection(collection);
-//                context.addConstraint(constraint);
-//            }
-//        };
-//        tomcat.addAdditionalTomcatConnectors(httpConnector());
-//        return tomcat;
+//        Object object = new Object(this);
+//        object.addAdditionalTomcatConnectors(new Connector[] { httpConnector() });
+//        return (TomcatServletWebServerFactory)object;
 //    }
-//
+
 //    @Bean
 //    public Connector httpConnector() {
 //        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 //        connector.setScheme("http");
-//        //Connector监听的http的默认端口号
-//        //其中两个端口不能一样
 //        connector.setPort(8080);
 //        connector.setSecure(false);
-//        //监听到http的端口号后转向到的https的端口号,也就是项目配置的port
 //        connector.setRedirectPort(443);
 //        return connector;
 //    }
+    /**
+     * http重定向到https
+     * @return
+     */
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 }
